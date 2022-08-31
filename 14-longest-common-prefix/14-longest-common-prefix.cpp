52a7f854531fd4a08fcd33028b59be9f3ba1c3ac
ranjan0346 <88393082+ranjan0346@users.noreply.github.com>
2022-08-31 08:12:18 +0530

    Time: 4 ms (79.53%), Space: 9.2 MB (55.54%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -4,8 +4,7 @@ public:
         string ans= strs[0];
         int n=strs.size();
         for(int i=1; i<n; i++){
-            int n2=ans.size();
-            for(int j=0; j<n2; j++)
+            for(int j=0; j<ans.size(); j++)
             {
                 if(ans[j]!=strs[i][j]){
                     ans=ans.substr(0, j);
